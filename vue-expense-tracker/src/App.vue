4f048328985d9442089f53cd2ff1a092db4e3a31
Dhruba Roy <Drdroy98@gmail.com>
2025-03-12 00:18:25 -0400

    New Project Adding Files

---
+++ vue-expense-tracker/src/App.vue
@@ -0,0 +1,32 @@
+/* eslint-disable */
+<template>
+  <Header />
+  <div class="container">
+  <Balance />
+  <IncomeExpenses />
+  </div>
+</template>
+<script lang = 'ts'>
+import Header from './components/Header.vue';
+import Balance from './components/Balance.vue';
+import IncomeExpenses from './components/IncomeExpenses.vue';
+export default {
+  components:{
+    Header,
+    Balance,
+    IncomeExpenses,
+  },
+  name: 'App',
+  data() {
+    return {
+      
+      
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
