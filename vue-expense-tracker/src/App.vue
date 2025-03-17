99b398515edf081b7e3ec9f9d33b8959bc5c5368
Dhruba Roy <Drdroy98@gmail.com>
2025-03-17 14:48:16 -0400

    Project Completed and Uploaded to Git

--- vue-expense-tracker/src/App.vue
+++ vue-expense-tracker/src/App.vue
@@ -2,28 +2,98 @@
 <template>
   <Header />
   <div class="container">
-  <Balance />
-  <IncomeExpenses />
+    <Balance :total="+total" />
+    <IncomeExpenses :income="+income" :expenses="+expenses" />
+    <TransactionList :transactions="transactions" @transactionDeleted="handleTransactionDeleted" />
+    <AddTransaction @submittedTransaction="handleTransactionSubmitted"/>
   </div>
 </template>
-<script lang = 'ts'>
+<script lang = 'ts', setup>
 import Header from './components/Header.vue';
 import Balance from './components/Balance.vue';
 import IncomeExpenses from './components/IncomeExpenses.vue';
-export default {
-  components:{
-    Header,
-    Balance,
-    IncomeExpenses,
-  },
-  name: 'App',
-  data() {
-    return {
-      
-      
-    }
+import TransactionList from './components/TransactionList.vue';
+import AddTransaction from './components/AddTransaction.vue';
+
+import{useToast} from 'vue-toastification';
+import { ref, computed, onMounted } from 'vue';
+
+const toast = useToast();
+
+const transactions = ref([]);
+
+//Check Local Storage
+onMounted(() => {
+  const savedTransactions = JSON.parse(localStorage.getItem('transactions'));
+  if(savedTransactions){
+    transactions.value = savedTransactions;
   }
+
+})
+
+console.log(transactions.value); // Want to check value of each transaction
+
+//Total off all transaction amounts -- Get Total
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction) =>{
+    return acc + transaction.amount;
+  }, 0);
+});
+
+// Get Income
+const income = computed(() => {
+  return transactions.value.filter((transaction) => transaction.amount > 0)
+  .reduce((acc, transaction) =>{
+    return acc + transaction.amount;
+  }, 0).toFixed(2);
+});
+
+// Get Expenses
+const expenses = computed(() => {
+  return transactions.value.filter((transaction) => transaction.amount < 0)
+  .reduce((acc, transaction) =>{
+    return acc + transaction.amount;
+  }, 0).toFixed(2);
+});
+
+//Add Transactions
+
+const handleTransactionSubmitted = (transactionData) => {
+  console.log(transactionData); // Log the field data
+  transactions.value.push({
+    id: generateUniqueId(),
+    text: transactionData.text,
+    amount: transactionData.amount,
+  });
+  console.log(generateUniqueId());
+  saveDataToLocalStorage();
+  toast.success('Transaction Added');
+};
+
+//Unique Id for each transaction
+
+const generateUniqueId = () => {
+  return Math.floor(Math.random() * 1000000);
 }
+
+//Delete Transaction
+
+const handleTransactionDeleted = (id) => {
+  transactions.value = transactions.value.filter((transaction) =>
+  transaction.id !== id);
+  
+  saveDataToLocalStorage();
+  toast.success('Transaction deleted');
+
+  console.log(id);
+
+};
+
+//Save to Local Storage
+const saveDataToLocalStorage = () =>{
+  localStorage.setItem('transactions', JSON.stringify(transactions.value));
+}
+
 
 </script>
 
---
+++ vue-expense-tracker/src/components/AddTransaction.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <h3>Add New Transaction</h3>
+        <form id="form" @submit.prevent="onSubmit">
+            <div class="form-control">
+                <label for="text">Text</label>
+                <input type="text" id="text" v-model="text" placeholder="Enter text..." />
+            </div>
+            <div class="form-control">
+                <label for="amount">Amount<br />
+                (negative - expense, positive - income)</label>
+            <input type="text" id="amount" v-model="amount" placeholder="Enter amount..."/>
+            </div>
+            <button class="btn">Add Transaction</button>
+        </form>
+    </div>
+</template>
+
+<script setup>
+
+import {ref} from 'vue';
+import{useToast} from 'vue-toastification';
+const text = ref('');
+const amount = ref('');
+
+const toast = useToast();
+
+const emit = defineEmits(['submittedTransaction']);
+
+
+    const onSubmit = () => {
+        if(!text.value || !amount.value){
+            //Toast notification error for fields
+            toast.error('Both fields must be filled'); 
+            return;
+        };
+
+    const transactionData = {
+            text: text.value,
+            amount: parseFloat(amount.value),
+        };
+        emit('submittedTransaction', transactionData);
+
+        //Clear fields from form after submit
+        text.value ='';
+        amount.value ='';
+    };
+
+</script>
